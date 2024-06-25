66ed47e2dd155a32eed1f814043dfbe4ad23b820
tarosjk <tarosjk@gmail.com>
2024-06-25 14:41:59 +0900

    worksページとモーダルの導入

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -1,8 +1,15 @@
 // $()と同義
 // $(function(){})
 jQuery(function ($) {
+  // ハンバーガーメニュー
   // $('.header-button').click(function(){})
   $('.header-button').on('click', () => {
     $('body').toggleClass('open')
   })
+
+  // modaal
+  $('.gallery').modaal({
+    type: 'image',
+  });
+
 })
